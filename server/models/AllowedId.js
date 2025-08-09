db7a1959f43c3a4a3421bb9fca6edf9937e2e5e6
ByteDance <xulan_serena@sjtu.edu.cn>
2025-08-09 23:06:58 +0800

    fix(AllowedId): map to actual DB column name

--- server/models/AllowedId.js
+++ server/models/AllowedId.js
@@ -3,22 +3,22 @@ const { DataTypes } = require('sequelize');
 const sequelize = require('../db');
 
 const AllowedId = sequelize.define('AllowedId', {
-  // ⚠️ 这里的 field 要和数据库里【真实列名】一致
+  // 属性名用 camelCase，field 指向真实列名（也是 camelCase）
   playerId: {
     type: DataTypes.STRING,
     allowNull: false,
     unique: true,
-    field: 'playerId',   // ← 如果 \d 显示是 playerId，就用它
+    field: 'playerId',     // ← 你的表里就是 "playerId"
   },
   used: {
     type: DataTypes.BOOLEAN,
     defaultValue: false,
-    field: 'used',       // 通常就是 used
+    field: 'used',         // ← 表里就是 "used"
   },
 }, {
-  tableName: 'allowed_ids', // 表名已经存在，无需改
-  timestamps: false,
-  underscored: false,       // 不强制 snake_case（避免再自动映射）
+  tableName: 'allowed_ids', // 你的表名
+  timestamps: true,         // 因为表里有 createdAt / updatedAt
+  underscored: false,       // 列是驼峰，不用下划线
 });
 
 module.exports = AllowedId;
